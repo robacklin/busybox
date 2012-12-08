7b0fccc72b244aaf491a01a0292b6682eb608904
robacklin <rob.acklin@gmail.com>
2012-12-08 13:39:31 -0500

    added build script

---
+++ build-busybox.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -d "moonshine" ] ; then
+	cd moonshine ; git pull 
+	cd ..
+else
+	git clone git://github.com/robacklin/moonshine.git
+fi
+
+sh ./moonshine/scripts/build-busybox.sh
